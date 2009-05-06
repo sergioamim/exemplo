aef7501f50e46f6eb1b258c2d99f601c4506e7cd
Sergio Amim <sergioamim@sergioamim-laptop.(none)>
2009-05-06 19:44:56 -0300

    Não lembro oq alterei :P

--- src/main/java/br/com/exemplo/apresentacao/EnqueteManageBean.java
+++ src/main/java/br/com/exemplo/apresentacao/EnqueteManageBean.java
@@ -19,90 +19,89 @@ import br.com.exemplo.dominio.repositorio.EnqueteRepositorio;
 @Name("enqueteManageBean")
 @Scope(ScopeType.PAGE)
 public class EnqueteManageBean {
-
-	@In(create = true)
+	
+	@In(create=true)
 	private EnqueteRepositorio enqueteRepositorio;
-
-	@In(create = true)
-	@Out(value = "enquete", required = false)
+	
+	@In(create=true)
+	@Out(value="enquete",required=false)
 	@DataModelSelection
 	private Enquete enquete;
-
+	
+	private String pesquisaNome;
+	
 	@DataModel
 	private List<Enquete> enquetes = new ArrayList<Enquete>();
-
-	private String pesquisaNome;
-
+	
 	@In
 	private FacesMessages facesMessages;
-
+	
 	private EstadoPaginaEnum estadoAtualPagina = EstadoPaginaEnum.PESQUISA;
-
-	public void salvar() {
-		try {
-			definirInclusaoOuEdicao();
-			pesquisar();
-		} catch (EntidadeJaCadastradaException e) {
-			facesMessages.addToControl("txtPergunta", e.getMessage());
+	
+	public void novo(){
+		enquete = new Enquete();
+		estadoAtualPagina = EstadoPaginaEnum.INCLUSAO;
+	}
+	
+	public void salvar(){
+		try{
+		definirInclusaoOuEdicao();
+		pesquisar();
+		}
+		catch (EntidadeJaCadastradaException e) {
+			facesMessages.addToControl("txtNome",e.getMessage());
 		}
 	}
-
-	public void pesquisar() {
+	
+	public void excluir(){
+		enqueteRepositorio.remover(enquete);
+		facesMessages.add("Enquete " + enquete.getPergunta() + " excluída com sucesso!");
+		pesquisar();
+	}
+	
+	public void  editar(){
+		estadoAtualPagina = EstadoPaginaEnum.EDICAO;
+	}
+	
+	public void pesquisar(){
 		estadoAtualPagina = EstadoPaginaEnum.PESQUISA;
 		enquetes.clear();
 		enquetes.addAll(enqueteRepositorio.consultarPorPergunta(pesquisaNome));
 		pesquisaNome = "";
 	}
-
-	public void excluir() {
-		enqueteRepositorio.remover(enquete);
-		facesMessages.add("Enquete " + enquete.getPergunta()
-				+ " excluída com sucesso!");
-		pesquisar();
+	
+	
+	private void definirInclusaoOuEdicao() throws EntidadeJaCadastradaException{
+		if(enquete.getId() == null){
+			enqueteRepositorio.adicionar(enquete);
+			facesMessages.add("Marca #{enquete.pergunta} incluída com sucesso!");
+		}
+		else{
+			enqueteRepositorio.atualizar(enquete);
+			facesMessages.add("Enquete #{enquete.pergunta} autualizada com sucesso!");
+		}
+	
 	}
-
-	public void novo() {
-		enquete = new Enquete();
-		estadoAtualPagina = EstadoPaginaEnum.INCLUSAO;
-	}
-
-	public void carregarEnquetes() {
-		if (enquetes.size() == 0) {
+	
+	
+	public void carregarEnquetes(){
+		if(enquetes.size()==0){
 			enquetes.clear();
 			enquetes.addAll(enqueteRepositorio.listar());
 		}
 	}
-
-	private void definirInclusaoOuEdicao() throws EntidadeJaCadastradaException {
-		if (enquete.getId() == null) {
-			enqueteRepositorio.adicionar(enquete);
-			facesMessages
-					.add("Marca #{enquete.pergunta} incluida com sucesso!");
-		} else {
-			enqueteRepositorio.atualizar(enquete);
-			facesMessages
-					.add("Marca #{enquete.pergunta} autualizada com sucesso!");
-		}
-
-	}
-
-	public void editar() {
-		estadoAtualPagina = EstadoPaginaEnum.EDICAO;
-	}
-
+	
+	
 	public boolean isEstadoDePesquisa() {
-		return (estadoAtualPagina == null || estadoAtualPagina
-				.equals(EstadoPaginaEnum.PESQUISA));
+		return (estadoAtualPagina == null || estadoAtualPagina.equals(EstadoPaginaEnum.PESQUISA));
 	}
-
 	public boolean isEstadoDeInclusao() {
 		return estadoAtualPagina.equals(EstadoPaginaEnum.INCLUSAO);
 	}
-
 	public boolean isEstadoDeEdicao() {
 		return estadoAtualPagina.equals(EstadoPaginaEnum.EDICAO);
 	}
-	
+
 	public String getPesquisaNome() {
 		return pesquisaNome;
 	}
@@ -110,4 +109,6 @@ public class EnqueteManageBean {
 	public void setPesquisaNome(String pesquisaNome) {
 		this.pesquisaNome = pesquisaNome;
 	}
+	
+
 }
--- src/main/java/br/com/exemplo/dominio/repositorio/EnqueteRepositorio.java
+++ src/main/java/br/com/exemplo/dominio/repositorio/EnqueteRepositorio.java
@@ -54,4 +54,5 @@ public class EnqueteRepositorio {
 	public List<Enquete> listar(){
 		return dataMapper.listar(Enquete.class);
 	}
+	
 }
